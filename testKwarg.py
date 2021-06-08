b8fda5d0ab71bf520f5e1281f710fd342419c4b1
natwijaya19 <natwijaya19@gmail.com>
2021-06-08 20:34:18 +0700

    new commit 2

---
+++ testKwarg.py
@@ -0,0 +1,9 @@
+# concatenate.py
+def concatenate(**kwargs):
+    result = ""
+    # Iterating over the Python kwargs dictionary
+    for arg in kwargs.values():
+        result += arg
+    return result
+
+print(concatenate(a="Real", b="Python", c="Is", d="Great", e="!"))
